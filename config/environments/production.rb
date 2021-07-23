5a2f2dfa890a4c0061e9c1d43ebab62a692aa2b4
Garvey <hello@garvey.codes>
2021-07-23 23:19:49 +1000

    Configure for mailgun email delivery via heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,18 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  mailgun = Rails.application.credentials.mailgun
+  ActionMailer::Base.smtp_settings = {
+    port: mailgun[:MAILGUN_SMTP_PORT],
+    address: mailgun[:MAILGUN_SMTP_SERVER],
+    user_name: mailgun[:MAILGUN_SMTP_LOGIN],
+    password: mailgun[:MAILGUN_SMTP_PASSWORD],
+    domain: 'one-up-api.herokuapp.com',
+    authentication: :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: '1upbouldering.app', protocol: 'https' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -13,7 +25,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -46,7 +58,7 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -81,7 +93,7 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
