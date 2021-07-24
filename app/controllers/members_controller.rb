18e7998cbef6a226d4118ef30e26f171d5fce3dc
Garvey <hello@garvey.codes>
2021-07-25 02:13:30 +1000

    Roll DIY authorisation layer on members endpoints

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -13,39 +13,60 @@ class MembersController < ApplicationController
     # create member profile, address, photo, waiver signature
     # registration handled by devise
     # current_user matches
+    if current_user.id == @user.id
+      # save all data
+    else
+      unauthorised_response
+    end
+
   end
 
   def show
     # return selected member with role, profile, address, photo, waiver signature
     # admin required or current_user matches
-
-    render json: constructed_member
+    if current_user.admin_role? || current_user.id == @user.id
+      render json: constructed_member
+    else
+      unauthorised_response
+    end
   end
 
   def index
     # return list of all members for admin dashboard
     # admin required
-
-    @users = User.all
-                 .includes(:user_profile)
-                 .includes(:user_address)
-                 .includes(user_photo: { image_attachment: :blob })
-                 .includes(:signed_waivers)
-
-    render json: @users.map { |user| constructed_member(user) }
+    if current_user.admin_role?
+      @users = User.all
+                   .includes(:user_profile)
+                   .includes(:user_address)
+                   .includes(user_photo: { image_attachment: :blob })
+                   .includes(:signed_waivers)
+      render json: @users.map { |user| constructed_member(user) }
+    else
+      unauthorised_response
+    end
   end
 
   def update
     # change selected member profile details or other attributes
     # admin required or current_user matches
+    if current_user.admin_role? || current_user.id == @user.id
+
+    end
   end
 
   def destroy
     # delete selected member
-    # admin required
+    # admin required or current_user matches
+    if current_user.admin_role? || current_user.id == @user.id
+      
+    end
   end
 
   private
+
+  def unauthorised_response
+    render json: { message: "You're not authorised for that information." }, status: :unauthorized
+  end
 
   def set_user
     @user = User.find(params[:id])
