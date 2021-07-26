9641066324d247a8d349b93bf39dff33b2a5c4f9
garveycodes <hello@garvey.codes>
2021-07-26 13:52:36 +1000

    Implement checkin endpoints and seed/request testing

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,16 +1,25 @@
-class CheckinsController < ApplicationController
+class CheckinsController < MembersController
   def create
     # create a new check-in from POSTed email matched to user and add it to the log
     # no auth/auth required
   end
 
-  def show
-    # return latest check-in with profile picture and relevant information
-    # admin required
-  end
-
   def index
     # return full list of check-ins from the past week (interim timeframe)
     # admin required
+    return unauthorised_response unless admin_auth?
+
+    render json: all_checkins.map { |checkin| constructed_checkin(checkin) }
+  end
+
+  protected
+
+  def all_checkins
+    CheckIn.all
+  end
+
+  def constructed_checkin(checkin)
+    { user: constructed_member(checkin.user),
+      checkInTimestamp: checkin.created_at }
   end
 end
