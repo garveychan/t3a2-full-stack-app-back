386b1455d2198cf63c60c00deccd0dc3267ff227
garvey <79407762+garveychan@users.noreply.github.com>
2021-12-21 17:32:31 +1100

    Amend CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'https://boulderingcrm.netlify.app']
+    origins ['http://localhost:3000', 'https://bouldering-crm.netlify.app']
 
     resource '*',
              headers: %w[Authorization],
